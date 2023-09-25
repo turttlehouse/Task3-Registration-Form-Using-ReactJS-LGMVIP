04c14113c001d75b7095ed6f0a3d5cf74dbd53a6
turttlehouse <buidandrun464@gmail.com>
2023-09-25 18:15:06 +0545

    components created - Displaystudentlist,header,RegisterForm, and header made in headercomponent by applying styles as well and rendering the header component in parent component app.jsx

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='header'>
+          <h1>Student Enrollment Form</h1>
+        </div>
+      
+      </div>
+  )
+}
+
+export default Header
